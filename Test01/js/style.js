496250cf9630c39aefa49f200ec2d4aabddcb468
GraceNam <zeidgame@gmail.com>
2021-06-07 20:04:07 +0900

    docs : 수정본
    <description>
    + 슬라이드 코드 수정
    <issue-number>

--- Test01/js/style.js
+++ Test01/js/style.js
@@ -9,8 +9,39 @@ carouselImg.addEventListener("mouseout", function() {
 });
 
 // 이미지 슬라이드
-var clicker = document.querySelector(".card-body");
+var clicker1 = document.querySelector("#text-click1");
+var clicker2 = document.querySelector("#text-click2");
+var clicker3 = document.querySelector("#text-click3");
+var chk1 = new Boolean(false);
+var chk2 = new Boolean(false);
+var chk3 = new Boolean(false);
 
-clicker.addEventListener("click", function() {
-  clicker.style.transform = "translateY(164px)";
+clicker1.addEventListener("click", function() {
+  if(chk1) {
+    clicker1.style.transform = "translateY(164px)";
+    chk1 = false;
+  } else {
+    clicker1.style.transform = "translateY(0)";
+    chk1 = true;
+  }
+});
+
+clicker2.addEventListener("click", function() {
+  if(chk2) {
+    clicker2.style.transform = "translateY(164px)";
+    chk2 = false;
+  } else {
+    clicker2.style.transform = "translateY(0)";
+    chk2 = true;
+  }
+});
+
+clicker3.addEventListener("click", function() {
+  if(chk3) {
+    clicker3.style.transform = "translateY(164px)";
+    chk3 = false;
+  } else {
+    clicker3.style.transform = "translateY(0)";
+    chk3 = true;
+  }
 });
